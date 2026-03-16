a013e7aa85aa1d7812b2c47cce753bbeeaa581f6
Cooper <sayamrat.kt@gmail.com>
2026-03-16 17:03:34 +0700

    feat: init spring boot

---
+++ backend-java/src/test/java/com/hexslayer/HexslayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexslayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexslayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
